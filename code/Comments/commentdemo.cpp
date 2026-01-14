692abf0cfe5216a55ca178f021a183776521478d
Abhirup Das <23f2003574@ds.study.iitm.ac.in>
2026-01-14 18:16:17 +0530

    demonstration of comments and usage of new-line character

---
+++ code/Comments/commentdemo.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+// This is being written to demonstrate single line comment
+/* This is being used to show
+   multi-line comment
+*/
+
+int main(){
+    std::cout << "Hello World!" << std::endl;
+    return 0;
+    //Program ends here
+}
---
+++ code/FirstCppProgram/newline_usage.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+//This program shows how the newline character is used
+int main(){
+    std::cout << "Number 1" << std::endl;
+    std::cout << "Number 2";
+    std::cout << "Number 3" << std::endl;
+}
