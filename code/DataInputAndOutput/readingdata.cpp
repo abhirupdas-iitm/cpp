7d249fced144a2e49da7e37c77fb632d3be70f17
Abhirup Das <23f2003574@ds.study.iitm.ac.in>
2026-01-15 22:44:34 +0530

    reading data using various commands of the std command

---
+++ code/DataInputAndOutput/readingdata.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main(){
+int age;
+std::string name;
+
+std::cout << "Please type in your last name : " << std::endl;
+std::cin >> name;
+
+std::cout << "Please type in your age : " << std::endl;
+std::cin >> age;
+
+std::cout << "Hello " << name << "! You are " << age << " years old! YOU'RE AN UNC AHAAAAA" << std::endl;
+}
