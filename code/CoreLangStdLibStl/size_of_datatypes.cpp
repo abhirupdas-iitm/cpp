37766e49e85a6889ccf85d0f0e48ca8a40d98212
Abhirup Das <23f2003574@ds.study.iitm.ac.in>
2026-01-16 20:42:55 +0530

    using the sizeof() function to know the sizes of different obejts/variables

---
+++ code/CoreLangStdLibStl/size_of_datatypes.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main(){
+    int bike_count = 2;
+    int truck_count = 7;
+    int vehicle_count = bike_count + truck_count;
+    int narrowing_conversion_assignment = 2.9;
+
+    std::cout << "size of int : " << sizeof(int) << std::endl;
+    std::cout << "size of truck_count : " << sizeof(truck_count) << std::endl;
+}
