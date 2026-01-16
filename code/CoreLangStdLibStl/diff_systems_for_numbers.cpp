5ef7c357584783b740d5d669f3433fbc1301777c
Abhirup Das <23f2003574@ds.study.iitm.ac.in>
2026-01-16 19:20:15 +0530

    diff ways to represent a number using decimal, octal, hexadecimal and binary

---
+++ code/CoreLangStdLibStl/diff_systems_for_numbers.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main(){
+    int number1 = 15; //decimal
+    int number2 = 017; //octal
+    int number3 = 0x0f; //hexadecimal
+    int number4 = 0b00001111; //binary - C++14
+
+    std::cout << "Hello World!" << std::endl;
+    
+    std::cout << "Number1 is : " <<number1 << std::endl;
+    std::cout << "Number2 is : " <<number2 << std::endl;
+    std::cout << "Number3 is : " <<number3 << std::endl;
+    std::cout << "Number4 is : " <<number4 << std::endl;
+}
