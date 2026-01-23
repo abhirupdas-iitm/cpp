a3a2a41074fea6a82d6ee2021fccda8fee505ab4
Abhirup Das <23f2003574@ds.study.iitm.ac.in>
2026-01-23 21:10:02 +0530

    modified arithmetic_operations.cpp to add demonstration of the remainder function

--- code/CoreLangStdLibStl/arithmetic_operations.cpp
+++ code/CoreLangStdLibStl/arithmetic_operations.cpp
@@ -19,19 +19,29 @@ int sum {number1 + number2};
 int other_sum = number1 + number2 + number1;
 
 //Subtraction
-int difference = number1 - number2;
+int difference {number1 - number2};
+int other_diff = number1 - number2 - number2;
 
 //Product
 int product = number1*number2;
 
 //Quotient
-auto quotient = number1/number2;
+int quotient {number1/number2};
+int other_quotient = number1/17;
+
+//Remainder
+int rem {number1%number2};
+int other_rem = number2%number1;
 
 std::cout << "The sum is : " << sum << std::endl;
 std::cout << "Other sum is : " << other_sum << std::endl;
 std::cout << "Difference is : " << difference << std::endl;
+std::cout << "Other diff is : " << other_diff << std::endl;
 std::cout << "Product is : " << product << std::endl;
 std::cout << "Quotient is : " << quotient << std::endl;
+std::cout << "Other quotient is : " << other_quotient << std::endl;
+std::cout << "Remainder is : " << rem << std::endl;
+std::cout << "Other remainder is : " << other_rem << std::endl;
 }
 else{
     std::cout << "Wrong input! Re-run the program and try again" << std::endl;
