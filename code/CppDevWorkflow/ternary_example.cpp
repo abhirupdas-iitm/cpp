b866a9b7340c810d60ceec138de62d39af8d1962
Abhirup Das <23f2003574@ds.study.iitm.ac.in>
2026-01-26 18:15:22 +0530

    completed the conditional programming part

--- code/CppDevWorkflow/ternary_example.cpp
+++ code/CppDevWorkflow/ternary_example.cpp
@@ -1,5 +1,42 @@
 #include <iostream>
 
 int main(){
+    int max{};
 
+    int a{35};
+    int b{20};
+    //using if-else way
+
+    std::cout << std::endl;
+    std::cout << "using regular if " << std::endl;
+
+    if (a>b){
+        max = a;
+    }else{
+        max = b;
+    }
+
+    std::cout << "max : " << max << std::endl;
+
+    //using ternary operator
+    max = (a>b) ? a:b;
+    
+    std::cout << std::endl;
+    std::cout << "using ternary operator " << std::endl;
+
+    std::cout << "max : " << max << std::endl;
+    
+    //ternary initialization 
+    std::cout << std::endl;
+    std::cout << "speed" << std::endl;
+    bool fast = false;
+
+    int speed {fast ? 300:150};
+    //from the example below, we can see implicit conversion 
+    std::cout << "The speed is : " << speed << std::endl;
+    std::cout << std::endl;
+    auto max1 = (a>b)? 98.5f : b; //Ternary operator
+    std::cout << "max : " << max1 << std::endl;
+
+    return 0;
 }
