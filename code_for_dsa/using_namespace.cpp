9a2acd31bdf7a290d90ec39679e2d599af5aa3bb
Abhirup Das <23f2003574@ds.study.iitm.ac.in>
2026-03-25 19:33:01 +0530

    using namespace so that repitition of std is prevented

---
+++ code_for_dsa/using_namespace.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "Hello Azmuth!" << endl << "Hello Zenith!" << endl; 
+/*
+As it can be seen very clearly from the line above, stuff can be written 
+in such a way that multiple outputs can be written in the same line
+but whether they would be present in the same line or not, will depend on
+the usage of `endl` 
+*/
+
+    cout << "Hey Primus!";
+    return 0;
+}
