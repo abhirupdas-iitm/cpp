2886e72d5cb3f382ab03a06eb22cd7c845e48f78
Abhirup Das <23f2003574@ds.study.iitm.ac.in>
2026-03-25 20:20:24 +0530

    input and output demonstration, and also a note on 'bits/stdc++.h' which brings all libraries

---
+++ code_for_dsa/input_output.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+/*
+include <bits/stdc++.h> makes sure that all libraries are included which is
+normally fine, although it takes slightly more time than normal
+*/
+
+using namespace std;
+int main(){
+    int x, y;
+    cin >> x >> y;
+    cout << "Value of x: " << x << " and y: " << y;
+/*
+From the above line we can see that the input system can be modified
+so that multiple inputs can be taken from one single line of code
+*/
+    return 0;
+}
