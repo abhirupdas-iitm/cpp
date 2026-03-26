fa2d6bdcdc143c89d86a4316b71238e68a4f0209
Abhirup Das <23f2003574@ds.study.iitm.ac.in>
2026-03-27 00:07:14 +0530

    fixed a minor issue on the non-param_functions program

--- code_for_dsa/non-param_functions.cpp
+++ code_for_dsa/non-param_functions.cpp
@@ -2,13 +2,17 @@
 using namespace std;
 int sum(int num1, int num2){
     int num3 = num1 + num2;
+    cout << "Sum of two numbers: ";
     return num3;
 }
 
 int main(){
     int num1, num2;
-cin >> num1 >> num2;
-int res = sum(num1,num2);
+    cout << "Enter first number" << endl;
+    cin >> num1;
+    cout << "Enter second number" << endl;
+    cin >> num2;
+    int res = sum(num1,num2);
     cout << res;
     return 0;
 }
