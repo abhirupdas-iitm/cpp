d79317b2f8fccada6d6e55d847d677b34213224a
Abhirup Das <23f2003574@ds.study.iitm.ac.in>
2026-03-26 19:50:53 +0530

    a few string operations

---
+++ code_for_dsa/string_operations.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s = "Striver";
+    
+    // The .size() method is used to find the length of a string
+    int len = s.size();
+    
+    cout << s << endl;
+    cout << "Length: " << len << endl;
+
+    return 0;
+}
