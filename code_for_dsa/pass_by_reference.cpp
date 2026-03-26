21af2aaca213ba94af39c3599281609edbfdc9d3
Abhirup Das <23f2003574@ds.study.iitm.ac.in>
2026-03-27 01:12:13 +0530

    demonstrating pass by reference using an increment program

---
+++ code_for_dsa/pass_by_reference.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+//pass by value
+void doSomething(int &num){
+    cout << num << endl;
+    cout << "Increasing the number by 5" << endl;
+    num+=5;
+    cout << num << endl;
+    cout << "Increasing the number by 5" << endl;
+    num+=5;
+    cout << num << endl;
+    cout << "Increasing the number by 5" << endl;
+    num+=5;
+    cout << num << endl;
+}
+int main(){
+    int num = 10;
+    doSomething(num);
+    cout << "After performing operations: ";
+    cout << num << endl;
+    return 0;
+}
