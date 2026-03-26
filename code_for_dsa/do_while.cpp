804cc7c9cf375a2885c9b097d97435ebbd84da97
Abhirup Das <23f2003574@ds.study.iitm.ac.in>
2026-03-26 23:43:01 +0530

    demonstration of do while loop with a simple user-based example (same one used for while loop program)

---
+++ code_for_dsa/do_while.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int i;
+    cout << "Enter a number you would like to get the sum of digits for" << endl;
+    cin >> i;
+    int sum = 0;
+    int r = 0;
+    do{
+        r = i%10;
+        sum = sum + r;
+        i = i/10;
+    }while(i>0);
+    cout << "Sum of digits is: " << sum;
+    return 0;
+}
